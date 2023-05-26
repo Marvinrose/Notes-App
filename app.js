d5d94e3aec7ad739ab0fb349f34a498a0d84c48a
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-26 17:12:38 +0100

    Added background color to notesapp

--- app.js
+++ app.js
@@ -50,6 +50,13 @@ app.use("/", require("./server/routes/index"));
 app.use("/", require("./server/routes/auth"));
 app.use("/", require("./server/routes/dashboard"));
 
+
+// Handle 404
+app.get('*', function(req, res) {
+  //res.status(404).send('404 Page Not Found.')
+  res.status(404).render('404');
+})
+
 app.listen(port, () => {
   console.log(`Notes app listening on port ${port}`);
 });
