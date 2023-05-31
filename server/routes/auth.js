3f835871d76ea6dff9d26d4e043807ed7430f567
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2023-05-31 12:46:40 +0100

    Implemented logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -57,6 +57,18 @@ router.get("/login-failure", (req, res) => {
   res.send("Something went wrong..");
 });
 
+// Destroy user session
+router.get("/logout", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+      res.send("Error logging out..");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
 // Presist User Data after successful authentication
 passport.serializeUser(function (user, done) {
   done(null, user.id);
